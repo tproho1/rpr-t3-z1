4a64abb172e1f1e58505dc93cdc2ba55081d9ec8
tproho1 <taremagare1>
2018-11-01 12:19:39 +0100

    Ubacena klasa MobilniBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MobilniBroj extends TelefonskiBroj{
+    private int mobilnaMreza;
+    private String broj;
+
+    public MobilniBroj(int mobilnaMreza, String broj) {
+        this.mobilnaMreza=mobilnaMreza;
+        this.broj=broj;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public String ispisi() {
+        return "0" + mobilnaMreza + "/" +broj;
+    }
+}
