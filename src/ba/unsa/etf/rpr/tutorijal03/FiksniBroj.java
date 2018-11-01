8148dd4041db6e2a2f46342c79de9be7deeb5acb
tproho1 <taremagare1>
2018-11-01 12:08:53 +0100

    Ubacen enum class Grad, Napisana metoda publci String ispisi()

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -1,20 +1,48 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-import java.util.*;
+enum Grad {
+     BUGOJNO("030"), BUSOVACA("030"), DOBRETICI("030"), DONJIVAKUF("030"), FOJNICA("030"), GORNJIVAKUF("030"),
+     JAJCE("030"), KISELJAK("030"), KREŠEVO("030"), NOVITRAVNKI("030"), TRAVNIK("030"), VITEZ("030"),
+     ODŽAK("031"), SARAJEVO("033"), ZENICA("032"), LIVNO("034"), TUZLA("035"), MOSTAR("036"), BIHAC("037"),
+     GORAŽDE("038");
 
-public enum class Grad {
-            
+     private String pozivni;
+
+      Grad(String pozivni){
+          this.pozivni=pozivni;
+      }
+      public String getPozivni(){
+          return pozivni;
+      }
+
         }
 
 public class FiksniBroj extends TelefonskiBroj {
+    private Grad grad;
     private String broj;
 
-    public FiksniBroj(Grad grad, String broj){};
+    public FiksniBroj(Grad grad, String broj){
+        this.grad=grad;
+        this.broj=broj;
+    };
 
+    public Grad getGrad(){
+        return grad;
+    }
+    public void setGrad(){ this.grad=grad;}
+
+    public String getBroj() {
+        return broj;
+    }
+    public void setBroj(String broj) {
+        this.broj = broj;
+    }
 
     @Override
-    public String ispisi() {
-        return null;
+    public String ispisi(){
+        String ispis="";
+        ispis+=getGrad().getPozivni() + "/" + broj;
+        return ispis;
     }
 
     @Override
--- src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -1,7 +1,5 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-import java.util.*;
-
 abstract class TelefonskiBroj {
     public abstract String ispisi();
     public abstract int hashCode();
