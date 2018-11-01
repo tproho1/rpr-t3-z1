9addd55206ae641826aad2517ffa6b6ba151d720
tproho1 <taremagare1>
2018-11-01 12:24:47 +0100

    Ubacena klasa MedunarodniBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -0,0 +1,21 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MedunarodniBroj extends TelefonskiBroj {
+        private String drzava;
+        private String broj;
+
+    public MedunarodniBroj(String drzava, String broj){
+        this.drzava=drzava;
+        this.broj=broj;
+    }
+
+    @Override
+    public String ispisi() {
+        return drzava+broj;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
