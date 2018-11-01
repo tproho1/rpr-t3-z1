5bff2cb97f1dd0cd9d5032428cf9b556d6d4ebb7
tproho1 <taremagare1>
2018-11-01 11:18:49 +0100

    Priprema

---
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+import java.util.*;
+
+public enum class Grad {
+            
+        }
+
+public class FiksniBroj extends TelefonskiBroj {
+    private String broj;
+
+    public FiksniBroj(Grad grad, String broj){};
+
+
+    @Override
+    public String ispisi() {
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+
+
+}
---
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+import java.util.*;
+
+abstract class TelefonskiBroj {
+    public abstract String ispisi();
+    public abstract int hashCode();
+}
+
